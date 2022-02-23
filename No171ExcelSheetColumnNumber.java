652bd0d8587b8f984685da51f3c23352da167ebc
SP <30589096+sp04tw@users.noreply.github.com>
2022-02-22 21:21:38 -0500

    Update No171ExcelSheetColumnNumber.java

--- No171ExcelSheetColumnNumber.java
+++ No171ExcelSheetColumnNumber.java
@@ -1,11 +1,10 @@
 class Solution {
     public int titleToNumber(String columnTitle) {
-        //BA
-        //B: 2
-        //BA: 2*26+1
+        //subChar-'A'+1
+        //('Z'-'A'+1)*26^N
         int sum=0;
         for(int i=0;i<columnTitle.length();i++){
-            sum=(columnTitle.charAt(i)-'A'+1)+sum*26;
+            sum+=(columnTitle.charAt(i)-'A'+1)*(int)Math.pow(26,(columnTitle.length()-1-i));
         }
         
         return sum;
